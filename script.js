f88c9b963dede67a1f7d4607cc1f0de2c554a11b
abhinv <abhinavroy2099@gmail.com>
2023-08-30 13:19:44 +0530

    add the update img

--- script.js
+++ script.js
@@ -1,21 +1,24 @@
 const input_container = document.querySelector(".input-container");
+const display_container=document.querySelector(".display_container")
 const btn = document.querySelectorAll(".button");
+const user_select = document.querySelector(".user_select");
+const machine_select = document.querySelector(".machine_select");
+
 console.log("check");
-let choice = ["paper", "scissor", "rock"];
+let random = ["paper", "scissor", "rock"];
 let userChoice;
 let computerinput;
 
 function computerChoice() {
-  const randomIndex = Math.floor(Math.random() * choice.length);
-  return choice[randomIndex];
+  const randomIndex = Math.floor(Math.random() * random.length);
+  return random[randomIndex];
 }
-
 
 btn.forEach((buttons) => {
   buttons.addEventListener("click", function (e) {
     userChoice = buttons.getAttribute("data-choice");
     // console.log(userChoice);
-    
+
     checkWinner();
   });
 });
@@ -23,16 +26,40 @@ btn.forEach((buttons) => {
 function checkWinner() {
   computerinput = computerChoice();
   console.log(`userChoice ${userChoice} computerinput ${computerinput}`);
-  updateimg(userChoice);
-  updateimg(computerChoice);
-  
+  updateimg(user_select, userChoice);
+  updateimg(machine_select, computerinput);
+  // input_container.remove()
+
+
   if (userChoice === computerinput) {
     console.log("tie");
-    // input_container.remove();
-    input_container.classList.toggle("remove")
+    renderResult()
+  } else if (
+    (userChoice === "rock" && computerinput === "scissor") ||
+    (userChoice === "paper" && computerinput === "rock") ||
+    (userChoice === "scissor" && computerinput === "paper")
+  ) {
+    console.log("youwin");
+    renderResult()
   } else {
-    console.log("lose");
+    console.log("computerwin");
+    renderResult()
   }
 }
+function renderResult(){
+  input_container.classList.toggle("remove")
+  display_container.classList.toggle("show")
+  // playAgain.classList.toggle("show")
+  // winnerMessage  = ""
+}
 
-function
+function updateimg(select, choice) {
+  select.classList.remove("paper");
+  select.classList.remove("rock");
+  select.classList.remove("scissor");
+
+  const img = select.querySelector("img");
+  select.classList.add(choice);
+  img.src = `./assests/${choice}.png`;
+  img.alt = choice;
+}
